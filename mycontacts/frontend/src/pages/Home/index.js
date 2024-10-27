47d89e6123188f96d74b43eaff3a4ab77dbd0971
Ian Felix <ianfelix030@gmail.com>
2024-10-26 21:58:56 -0300

    feat: add filter

--- mycontacts/frontend/src/pages/Home/index.js
+++ mycontacts/frontend/src/pages/Home/index.js
@@ -1,40 +1,81 @@
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import arrow from '../../assets/icons/row.svg';
 import edit from '../../assets/icons/edit.svg';
 import trash from '../../assets/icons/trash.svg';
 import * as S from './styles';
 
 export default function Home() {
+  const [contacts, setContacts] = useState([]);
+  const [orderBy, setOrderBy] = useState('asc');
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(searchTerm.toLowerCase()),
+  );
+
+  useEffect(() => {
+    (async () => {
+      const request = await fetch(
+        `http://localhost:3333/contacts?orderBy=${orderBy}`,
+      );
+      const response = await request.json();
+      setContacts(response);
+    })();
+  }, [orderBy]);
+
+  function handleToggleOrderBy() {
+    setOrderBy((prevState) => (prevState === 'asc' ? 'desc' : 'asc'));
+  }
+
+  function handleChangeSearchTerm(event) {
+    setSearchTerm(event.target.value);
+  }
+
   return (
     <S.Container>
       <S.InputSearchContainer>
-        <input type="text" placeholder="Pesquisar contato..." />
+        <input
+          value={searchTerm}
+          onChange={handleChangeSearchTerm}
+          type="text"
+          placeholder="Pesquisar contato..."
+        />
       </S.InputSearchContainer>
       <S.Header>
-        <strong>3 contatos</strong>
+        <strong>
+          {filteredContacts.length}
+          {filteredContacts.length === 1 ? ' contato' : ' contatos'}
+        </strong>
         <Link to="/new">Novo contato</Link>
       </S.Header>
 
-      <S.ListContainer>
-        <header>
-          <button type="button" className="sort-button">
+      {filteredContacts.length > 0 && (
+        <S.ListHeader orderBy={orderBy}>
+          <button
+            type="button"
+            className="sort-button"
+            onClick={handleToggleOrderBy}
+          >
             <span>Nome</span>
             <img src={arrow} alt="Arrow" />
           </button>
-        </header>
+        </S.ListHeader>
+      )}
 
-        <S.Card>
+      {filteredContacts.map((contact) => (
+        <S.Card key={contact.id}>
           <div className="info">
             <div className="contact-name">
-              <strong>Ian Felix</strong>
-              <small>instagram</small>
+              <strong>{contact.name}</strong>
+              {!!contact.category && <small>{contact.category}</small>}
             </div>
-            <span>ianfelix030@gmail.com</span>
-            <span>(41) 99999-9999</span>
+            <span>{contact.email}</span>
+            <span>{contact.phone}</span>
           </div>
 
           <div className="actions">
-            <Link to="/edit/1">
+            <Link to={`/edit/${contact.id}`}>
               <img src={edit} alt="Edit" />
             </Link>
 
@@ -43,67 +84,7 @@ export default function Home() {
             </button>
           </div>
         </S.Card>
-        <S.Card>
-          <div className="info">
-            <div className="contact-name">
-              <strong>Ian Felix</strong>
-              <small>instagram</small>
-            </div>
-            <span>ianfelix030@gmail.com</span>
-            <span>(41) 99999-9999</span>
-          </div>
-
-          <div className="actions">
-            <a href="/edit/1">
-              <img src={edit} alt="Edit" />
-            </a>
-
-            <button type="button">
-              <img src={trash} alt="Delete" />
-            </button>
-          </div>
-        </S.Card>
-        <S.Card>
-          <div className="info">
-            <div className="contact-name">
-              <strong>Ian Felix</strong>
-              <small>instagram</small>
-            </div>
-            <span>ianfelix030@gmail.com</span>
-            <span>(41) 99999-9999</span>
-          </div>
-
-          <div className="actions">
-            <a href="/edit/1">
-              <img src={edit} alt="Edit" />
-            </a>
-
-            <button type="button">
-              <img src={trash} alt="Delete" />
-            </button>
-          </div>
-        </S.Card>
-        <S.Card>
-          <div className="info">
-            <div className="contact-name">
-              <strong>Ian Felix</strong>
-              <small>instagram</small>
-            </div>
-            <span>ianfelix030@gmail.com</span>
-            <span>(41) 99999-9999</span>
-          </div>
-
-          <div className="actions">
-            <a href="/edit/1">
-              <img src={edit} alt="Edit" />
-            </a>
-
-            <button type="button">
-              <img src={trash} alt="Delete" />
-            </button>
-          </div>
-        </S.Card>
-      </S.ListContainer>
+      ))}
     </S.Container>
   );
 }
