9b7453d530ce9fcd1de426e97e96491915a60f5a
Ian Felix <ianfelix030@gmail.com>
2024-10-26 21:54:09 -0300

    feat: adjust img

--- mycontacts/frontend/src/pages/Home/styles.js
+++ mycontacts/frontend/src/pages/Home/styles.js
@@ -47,22 +47,27 @@ export const Header = styled.header`
   }
 `;
 
-export const ListContainer = styled.div`
+export const ListHeader = styled.header`
   margin-top: 24px;
   margin-bottom: 8px;
-  header {
-    margin-bottom: 8px;
-    .sort-button {
-      background: transparent;
-      border: none;
-      display: flex;
-      align-items: center;
 
-      span {
-        color: ${({ theme }) => theme.colors.primary.main};
-        font-weight: bold;
-        margin-right: 8px;
-      }
+  margin-bottom: 8px;
+  .sort-button {
+    background: transparent;
+    border: none;
+    display: flex;
+    align-items: center;
+
+    span {
+      color: ${({ theme }) => theme.colors.primary.main};
+      font-weight: bold;
+      margin-right: 8px;
+    }
+
+    img {
+      transform: ${({ orderBy }) =>
+        orderBy === 'asc' ? 'rotate(180deg)' : 'rotate(0deg)'};
+      transition: transform 0.2s ease-in;
     }
   }
 `;
